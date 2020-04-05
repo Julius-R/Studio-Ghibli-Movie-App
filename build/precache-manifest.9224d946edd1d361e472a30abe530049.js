cf60ece98ce40e582f2e98d5777499cebc0f95ee
Julius-r <julius.robinson95@yahoo.com>
2020-04-05 18:14:23 -0500

    DEploy to netlifhy

---
+++ build/precache-manifest.9224d946edd1d361e472a30abe530049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e516bd3f733c4ec9bb6e42b6142491",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d355085155fda4372f02",
+    "url": "/static/css/main.25de3a12.chunk.css"
+  },
+  {
+    "revision": "902584acb67cd4182e44",
+    "url": "/static/js/2.1a867dea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a867dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d355085155fda4372f02",
+    "url": "/static/js/main.754c87d7.chunk.js"
+  },
+  {
+    "revision": "2e3ff883bfabc8de44d9",
+    "url": "/static/js/runtime-main.1741b459.js"
+  }
+]);
